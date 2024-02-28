441f90e88fb6b7d94c5361b62000a6f9c875bcd0
DKatsoulis12 <108552289+DKatsoulis12@users.noreply.github.com>
2024-02-28 23:56:42 +0000

    Add files via upload

---
+++ Full Version ALL TASKS FINISHED (REFURBISH NEEDED)/Chameleon.java
@@ -0,0 +1,54 @@
+import javafx.scene.paint.Color;
+import java.util.List;
+
+public class Chameleon extends Cell {
+    private static final int populationCamouflage = 2;
+
+    public Chameleon(Field field, Location loc, Color alivecol, Color deadcol) {
+        super(field, loc, alivecol, deadcol);
+    }
+
+    /*
+    public void act() {
+        // Always set Chameleon cells as alive
+        setAlive();
+
+        // Toggle between magenta and cyan in each generation
+        if (getColor().equals(Color.MAGENTA)) {
+            setColor(Color.CYAN);
+        } else {
+            setColor(Color.MAGENTA);
+        }
+
+        // Make sure to update the state after changing the color
+        setNextState(true);
+    }*/
+    
+    
+
+public void act() {
+    List<Cell> neighbours = getField().getLivingNeighbours(getLocation());
+    int neighboursSize = 0;
+    if(neighbours != null){
+        for (Cell neighbour : neighbours) {
+        if (neighbour instanceof Mycoplasma && neighbour.isAlive()) {
+            neighboursSize++;
+        }
+    }
+    }
+    //System.out.println("Neighbour Size: " + neighboursSize);
+    if (isAlive()) {
+        if (neighboursSize >= populationCamouflage) {
+            setAliveColor(Color.CYAN);
+        } else {
+            setAliveColor(Color.MAGENTA);
+        }
+    }
+}
+
+    @Override
+    public void updateState() {
+        // Override updateState to ensure all Chameleon cells stay alive
+        setAlive();
+    }
+}
---
+++ Full Version ALL TASKS FINISHED (REFURBISH NEEDED)/NonDeterministic.java
@@ -0,0 +1,72 @@
+import javafx.scene.paint.Color;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * Write a description of class NonDeterminstic here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class NonDeterministic extends Mycoplasma
+{
+    
+   // instance variables - replace the example below with your own
+    
+public NonDeterministic(Field field, Location location, Color alivecol, Color deadcol) {
+    super(field, location, alivecol, deadcol);
+    this.isDiseased = false;
+    this.nextDiseased = false;
+    this.diseaseAbility = true;
+}
+
+    /**
+    * This is how the NonDeterministic decides if it's alive or not
+    */
+    public void act() {
+    List<Cell> neighbours = getField().getLivingNeighbours(getLocation());
+    neighbourCount = neighbours.size();
+    fieldStats = SimulatorView.getFieldStats();
+
+    Random rand = Randomizer.getRandom();
+    double rng = rand.nextDouble();
+    //System.out.println("Before act(): Cell at " + getLocation() + " isAlive: " + isAlive());
+    System.out.println("Cell at " + getLocation() + " has rng value: " + rng);
+    
+    determineDisease();
+    
+    if (isDiseased) {
+            actDiseased();
+    } else {
+        if (rng <= 0.38) {
+            if (isAlive()) {
+                tempState = aliveCell(3,4);
+                if (!tempState) {
+                    //fieldStats.decrementCount(this.getClass());
+                }   
+            } else {
+                 tempState = reproduction(4);
+                 if (tempState) {
+                    //fieldStats.incrementCount(this.getClass());
+                 }
+            }
+        } else {
+            if (isAlive()) {
+                tempState = aliveCell(2,5);
+                if (!tempState) {
+                    //fieldStats.decrementCount(this.getClass());
+                } 
+            } else {
+                tempState = reproduction(2);
+                if (tempState) {
+                    //fieldStats.incrementCount(this.getClass());
+                }
+            }
+    }
+    setNextState(tempState);
+    //System.out.println("After act(): Cell at " + getLocation() + " isAlive: " + isAlive());
+    }
+    
+}
+
+}
